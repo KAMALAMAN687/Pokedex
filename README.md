d3ce25f6b39cc962ab8212804b3a4bda9a9b9e85
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-02 01:29:55 +0530

    add some instructions in the readme file

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Clone the Repo.
+
+First of all clone the repo and go to the destination of the saved file .
+
+Now press cd Pokedex in terminal
+
+after that install node modules - npm install
+
+# Run the Project
+
+Run command - npm run dev
--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./PokemonDetails.css";
+import Loader from "../Loader/Loader";
 function PokemonDetails() {
   const { id } = useParams();
   const [pokemon, setPokemon] = useState({});
+  const [loading, setLoading] = useState(true);
   async function downloadPokemon() {
+    setLoading(true);
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
     setPokemon({
       name: response.data.name,
@@ -15,6 +18,7 @@ function PokemonDetails() {
       height: response.data.height,
       types: response.data.types.map((t) => t.type.name),
     });
+    setLoading(false);
     console.log(pokemon);
   }
   useEffect(() => {
@@ -26,25 +30,28 @@ function PokemonDetails() {
       <Link to="/">
         <h1 id="pokedex-detail-heading">Pokedex</h1>
       </Link>
-
-      <div className="detail-wrapper">
-        <div className="detail-image">
-          <img src={pokemon.image} alt="Not Able To Load Image" />
-        </div>
-        <div className="detail">
-          <h1 className="detail-name">{pokemon.name}</h1>
-          <h1 className="detail-height">Height : {pokemon.height}</h1>
-          <h1 className="detail-weight">Weight : {pokemon.weight}</h1>
-          <div className="type-div">
-            {pokemon.types &&
-              pokemon.types.map((e) => (
-                <div key={e} className="type">
-                  {e}
-                </div>
-              ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="detail-wrapper">
+          <div className="detail-image">
+            <img src={pokemon.image} alt="Not Able To Load Image" />
+          </div>
+          <div className="detail">
+            <h1 className="detail-name">{pokemon.name}</h1>
+            <h1 className="detail-height">Height : {pokemon.height}</h1>
+            <h1 className="detail-weight">Weight : {pokemon.weight}</h1>
+            <div className="type-div">
+              {pokemon.types &&
+                pokemon.types.map((e) => (
+                  <div key={e} className="type">
+                    {e}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
